a8245f03d0d3bb1ccff64f174e5ef921ee2c4fea
raufwasimiqbalahmed <andrew.sim84@gmail.com>
2024-07-17 22:29:49 +0200

    Added view model  and some renaming

---
+++ Set/ModelView/setGame.swift
@@ -0,0 +1,16 @@
+//
+//  setGame.swift
+//  Set
+//
+//  Created by Rauf Wasim Iqbal Ahmed on 17.07.24.
+//
+
+import Foundation
+
+class SetGame
+{
+    private var theSetGame:Game
+    init() {
+        self.theSetGame = Game()
+    }
+}
---
+++ Set/View/SetGameView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by Rauf Wasim Iqbal Ahmed on 17.07.24.
+//
+
+import SwiftUI
+
+struct SetGameView: View {
+    let theGameView:SetGame
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        let game = SetGame()
+
+        Group {
+            SetGameView(theGameView: game)
+                .preferredColorScheme(.light)
+                .previewDisplayName("Light Theme")
+            
+            SetGameView(theGameView: game)
+                .preferredColorScheme(.dark)
+                .previewDisplayName("Dark Theme")
+        }
+    }
+}
