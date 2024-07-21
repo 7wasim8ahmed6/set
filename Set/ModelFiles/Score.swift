baa6b22980c1e97165d77180a504d48ea3bcc834
raufwasimiqbalahmed <andrew.sim84@gmail.com>
2024-07-21 14:45:01 +0200

    Added scoring logic

---
+++ Set/ModelFiles/Score.swift
@@ -0,0 +1,44 @@
+//
+//  Score.swift
+//  Set
+//
+//  Created by Rauf Wasim Iqbal Ahmed on 21.07.24.
+//
+
+import Foundation
+
+struct Score {
+    private(set) var points = 0
+    private var consecutiveCorrect = 0
+    private var startTime: Date?
+    private let predefinedTimeLimit: TimeInterval = 60 // 60 seconds
+
+    mutating func startTimer() {
+        startTime = Date()
+    }
+
+    mutating func stopTimerAndCalculateBonus() {
+        guard let startTime = startTime else { return }
+        let timeTaken = Date().timeIntervalSince(startTime)
+        let timeSaved = predefinedTimeLimit - timeTaken
+        if timeSaved > 0 {
+            let bonusPoints = Int(timeSaved / 30)
+            points += bonusPoints
+        }
+    }
+
+    mutating func addCorrectPoints() {
+        points += 3
+        if consecutiveCorrect != 0 {
+            points += consecutiveCorrect + 1
+        }
+        consecutiveCorrect += 1
+        stopTimerAndCalculateBonus()
+        startTimer()
+    }
+
+    mutating func deductPoints() {
+        points -= 1
+        consecutiveCorrect = 0
+    }
+}
