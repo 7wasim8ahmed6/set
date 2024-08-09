eadbcbc0a0c88ec2851d70e7a3932fab1646bf68
raufwasimiqbalahmed <andrew.sim84@gmail.com>
2024-08-09 06:24:14 +0200

    comment welcome and finish game screen

--- Set/View/SetGameView.swift
+++ Set/View/SetGameView.swift
@@ -19,7 +19,7 @@ struct SetGameView: View {
         static let MaxAspectGridCards = 16
     }
     
-    func GameCardView(_ card: Card) -> some View {
+    private func GameCardView(_ card: Card) -> some View {
         theGameView.interpretCard(aCard: card)
             .aspectRatio(GameViewConsts.AspectRatio, contentMode: .fit)
             .padding(GameViewConsts.CardPadding)
@@ -29,40 +29,49 @@ struct SetGameView: View {
             }
     }
     
-    var body: some View {
-        VStack {
-            if showWelcomeScreen {
-                WelcomeView(showWelcomeScreen: $showWelcomeScreen, welcomeMessage: theGameView.getWelcomeMessage())
-            }
-            else{
-                if theGameView.theSetGame.mGameFinished {
-                    GameFinishView(points: theGameView.theSetGame.mScore.points, newGame: theGameView.newGame)
-                } else {
-                    // Top Bar with Timer, Info and Hint Button
-                    TopBarView(timeRemaining: theGameView.timeRemaining, newGame: theGameView.newGame, Pts: theGameView.theSetGame.mScore.points)
-                    
-                    if theGameView.theSetGame.mDrawCards.count <= GameViewConsts.MaxAspectGridCards {
-                        AspectVGrid(items: theGameView.theSetGame.mDrawCards, aspectRatio: GameViewConsts.AspectRatio) { card in
+    private var MainView : some View
+    {
+        VStack{
+            // Top Bar with Timer, Info and Hint Button
+            TopBarView(timeRemaining: theGameView.timeRemaining, newGame: theGameView.newGame, Pts: theGameView.theSetGame.mScore.points)
+            
+            if theGameView.theSetGame.mDrawCards.count <= GameViewConsts.MaxAspectGridCards {
+                AspectVGrid(items: theGameView.theSetGame.mDrawCards, aspectRatio: GameViewConsts.AspectRatio) { card in
+                    GameCardView(card)
+                }
+            } else {
+                ScrollView {
+                    LazyVGrid(columns: [GridItem(.adaptive(minimum: GameViewConsts.GridCardMinSize), spacing: 0)], spacing: 0) {
+                        ForEach(theGameView.theSetGame.mDrawCards) { card in
                             GameCardView(card)
                         }
-                    } else {
-                        ScrollView {
-                            LazyVGrid(columns: [GridItem(.adaptive(minimum: GameViewConsts.GridCardMinSize), spacing: 0)], spacing: 0) {
-                                ForEach(theGameView.theSetGame.mDrawCards) { card in
-                                    GameCardView(card)
-                                }
-                            }
-                        }
                     }
-                    
-                    // Bottom Bar with Draw Cards and Start Fresh Button
-                    BottomBarView(InfoString: theGameView.getScoringInfo(), drawCards: {
-                        theGameView.drawCards()
-                    }, provideHint: theGameView.provideHint)
                 }
             }
+            
+            // Bottom Bar with Draw Cards and Start Fresh Button
+            BottomBarView(InfoString: theGameView.getScoringInfo(), drawCards: {
+                theGameView.drawCards()
+            }, provideHint: theGameView.provideHint)
         }
         .padding(.horizontal)
+    }
+    
+    var body: some View {
+//        VStack {
+//            if showWelcomeScreen {
+//                WelcomeView(showWelcomeScreen: $showWelcomeScreen, welcomeMessage: theGameView.getWelcomeMessage())
+//            }
+//            else{
+//                if theGameView.theSetGame.mGameFinished {
+//                    GameFinishView(points: theGameView.theSetGame.mScore.points, newGame: theGameView.newGame)
+//                } else {
+//                    MainView
+//                }
+//            }
+            MainView
+//        }
+//        .padding(.horizontal)
     }
 }
 
